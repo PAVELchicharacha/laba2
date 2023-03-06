cb4163c9efe80c5c3cecb29c4696373753815015
павел <@DESKTOP-SO04HOV>
2023-03-06 23:06:26 +0300

    Добавьте файлы проекта.

---
+++ laba2/DateConstructor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba2
+{
+    internal class DateConstructor
+    {
+        private int year;
+        private int month;
+        private int day;
+        private DateTime time;
+        public int Year { get; set; }
+        public int Month { get { return month; } set { if (value <= 12) month = value; } }
+        public int Day { get { return day; } set { if (value <= 31) Day= value; } }
+
+        public DateConstructor( int year, int month, int day)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+        }
+        public override string ToString()
+        {
+            return Year + " " + Month + " " + Day;
+        }
+    }
+}
---
+++ laba2/Payment.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba2
+{
+    internal class Payment
+    {
+        public string Name;
+        public double Salary;
+        public DateTime Year;
+        public double Allowance;
+        public double Tax;
+        public double DaysInMonth; //кол-во рабочих дней
+        public double WorkingDaysmounth; //кол-во отработанных дней
+        public double Accruedamount; //начисленная сумма
+        public double Amountwithheld; //удержанная сумма
+
+        public Payment(string name, double salary, DateTime year, double allowance, double tax, double daysInMonth, double workingDaysmounth, double accruedamount, double amountwithheld)
+        {
+            Name = name;
+            Salary = salary;
+            Year = year;
+            Allowance = allowance;
+            Tax = tax;
+            DaysInMonth = daysInMonth;
+            WorkingDaysmounth = workingDaysmounth;
+            Accruedamount = accruedamount;
+            Amountwithheld = amountwithheld;
+        }
+
+        public double GetSalary()
+        {
+            return (WorkingDaysmounth/DaysInMonth*Salary);
+        }
+        public double GetAmountWithheld()
+        {
+            return (Accruedamount / 100 * 99) / 100 * 87;
+        }
+        public TimeSpan GetYear()
+        {
+            return DateTime.Now.Subtract(Year);
+        }
+
+    }
+}
